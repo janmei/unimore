c5add8cf8db70d8a08d6ed605ec5475e55995525
Meininghaus, Jan <Jan.Meininghaus@dvag.com>
2019-11-06 13:47:01 +0100

    changes

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ const express = require("express");
 const server = express();
 const port = 3000;
 
-server.use("/", express.static("public"));
+server.use("/", express.static(__dirname + "/public"));
 
 server.listen(port, () =>
   console.log(`Example server listening on port ${port}!`)
